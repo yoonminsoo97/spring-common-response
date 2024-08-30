f015c1b432887595bd1031469cba81a6a5a864da
yoonminsoo97 <yoonminwater@gmail.com>
2024-08-31 08:51:42 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<div align="center">
+    <h1>API 요청 성공 및 예외 발생 시 공통 응답 처리</h1>
+</div>
---
+++ src/main/java/com/example/response/SpringCommonResponseExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.response;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommonResponseExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCommonResponseExampleApplication.class, args);
+    }
+
+}
