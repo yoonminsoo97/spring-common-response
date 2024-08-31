29b5a4d51356ba81af59692fc53359f2bd023422
yoonminsoo97 <yoonminwater@gmail.com>
2024-09-01 01:07:07 +0900

    feat: 전역 예외 처리 핸들러 추가

---
+++ src/main/java/com/example/response/global/error/handler/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.example.response.global.error.handler;
+
+import com.example.response.global.common.dto.ApiResponse;
+import com.example.response.global.error.dto.ErrorResponse;
+import com.example.response.global.error.exception.BaseException;
+import com.example.response.global.error.exception.ErrorType;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ApiResponse<ErrorResponse>> handle(BaseException e) {
+        final ErrorType errorType = e.getErrorType();
+        final ErrorResponse errorResponse = ErrorResponse.of(errorType);
+        return ResponseEntity.status(errorType.getHttpStatus()).body(ApiResponse.fail(errorResponse));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<ErrorResponse>> handle(MethodArgumentNotValidException e) {
+        final ErrorType errorType = ErrorType.INVALID_INPUT_VALUE;
+        final ErrorResponse errorResponse = ErrorResponse.of(errorType, e.getBindingResult());
+        return ResponseEntity.badRequest().body(ApiResponse.fail(errorResponse));
+    }
+
+}
